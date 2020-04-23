5479db5a3912672c9e74ef9dfa2d9ab822186822
Maick Souza <maick_a_s@msn.com>
2020-04-23 08:04:05 -0300

    basic07

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-import background from './assets/background.jpg';
 
 import Header from './components/Header';
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
 
   function handleAddProject() { 
-
     setProjects([...projects, `Novo projeto ${Date.now()}`]);
-
-    console.log(projects);
   }
 
   return (
@@ -20,12 +23,12 @@ function App() {
       <Header title="Projetos" />
 
       <ul>
-        {projects.map(project => (<li key={project}>{project}</li>))}
+        {projects.map(project => (<li key={project.id}>{project.title}</li>))}
       </ul>
     
       <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
 
-      <img width={300} src={background} />
+
     </>
   );
 }
